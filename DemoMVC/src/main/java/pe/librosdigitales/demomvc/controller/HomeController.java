050f27bc4075ae8e8973b4422d0a922b5f40b5c6
Gustavo Coronel <gcoronelc@gmail.com>
2015-10-15 22:22:00 -0500

    Demo.

---
+++ DemoMVC/src/main/java/pe/librosdigitales/demomvc/controller/HomeController.java
@@ -0,0 +1,53 @@
+package pe.librosdigitales.demomvc.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import pe.librosdigitales.demomvc.service.MateService;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired
+	private MateService mateService;
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "suma";
+	}
+	
+	@RequestMapping(value = "sumar.htm", method = RequestMethod.POST)
+	public String sumar(@RequestParam("n1") int n1, @RequestParam("n2") int n2, Model model){
+		int suma = mateService.sumar(n1, n2);
+		model.addAttribute("suma", suma);
+		return "suma";
+	}
+	
+}
---
+++ DemoMVC/src/main/java/pe/librosdigitales/demomvc/service/MateService.java
@@ -0,0 +1,12 @@
+package pe.librosdigitales.demomvc.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MateService {
+
+	public int sumar(int n1, int n2){
+		return (n1+n2);
+	}
+	
+}
